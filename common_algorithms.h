be5932f5913e9f6a348ef2eddfab0f53ff834f9f
Jan Kašpar <jan.kaspar@cern.ch>
2019-03-07 20:48:03 +0100

    Switched to regression formula also in y.

--- common_algorithms.h
+++ common_algorithms.h
@@ -15,41 +15,36 @@ Kinematics DoReconstruction(const HitData &h, const Environment & env)
 
 	// ----- single-arm kinematics reconstruction -----
 
-	double D_x_L = - env.v_x_L_1_F * env.L_x_L_2_F + env.v_x_L_2_F * env.L_x_L_1_F;
-	k.th_x_L = (env.v_x_L_1_F * h.L_2_F.x - env.v_x_L_2_F * h.L_1_F.x) / D_x_L;
-	k.vtx_x_L = (- h.L_1_F.x * env.L_x_L_2_F + h.L_2_F.x * env.L_x_L_1_F) / D_x_L;
-
-	double D_x_R = + env.v_x_R_1_F * env.L_x_R_2_F - env.v_x_R_2_F * env.L_x_R_1_F;
-	k.th_x_R = (env.v_x_R_1_F * h.R_2_F.x - env.v_x_R_2_F * h.R_1_F.x) / D_x_R;
-	k.vtx_x_R = (+ h.R_1_F.x * env.L_x_R_2_F - h.R_2_F.x * env.L_x_R_1_F) / D_x_R;
-
-	/*
 	k.th_x_L_1_F = - h.L_1_F.x / env.L_x_L_1_F;
   	k.th_x_L_2_F = - h.L_2_F.x / env.L_x_L_2_F;
-  	k.th_x_L = (k.th_x_L_1_F + k.th_x_L_2_F) / 2.;
+  	//k.th_x_L = (k.th_x_L_1_F + k.th_x_L_2_F) / 2.;
   	
 	k.th_x_R_1_F = + h.R_1_F.x / env.L_x_R_1_F;
   	k.th_x_R_2_F = + h.R_2_F.x / env.L_x_R_2_F;
-  	k.th_x_R = (k.th_x_R_1_F + k.th_x_R_2_F) / 2.;
+  	//k.th_x_R = (k.th_x_R_1_F + k.th_x_R_2_F) / 2.;
 
-	k.vtx_x_L = 0.;
-	k.vtx_x_R = 0.;
-	*/
+	const double D_x_L = - env.v_x_L_1_F * env.L_x_L_2_F + env.v_x_L_2_F * env.L_x_L_1_F;
+	k.th_x_L = (env.v_x_L_1_F * h.L_2_F.x - env.v_x_L_2_F * h.L_1_F.x) / D_x_L;
+	k.vtx_x_L = (- h.L_1_F.x * env.L_x_L_2_F + h.L_2_F.x * env.L_x_L_1_F) / D_x_L;
+
+	const double D_x_R = + env.v_x_R_1_F * env.L_x_R_2_F - env.v_x_R_2_F * env.L_x_R_1_F;
+	k.th_x_R = (env.v_x_R_1_F * h.R_2_F.x - env.v_x_R_2_F * h.R_1_F.x) / D_x_R;
+	k.vtx_x_R = (+ h.R_1_F.x * env.L_x_R_2_F - h.R_2_F.x * env.L_x_R_1_F) / D_x_R;
 
 	k.th_y_L_1_F = - h.L_1_F.y / env.L_y_L_1_F;
   	k.th_y_L_2_F = - h.L_2_F.y / env.L_y_L_2_F;
-  	k.th_y_L = (k.th_y_L_1_F + k.th_y_L_2_F) / 2.;
+  	//k.th_y_L = (k.th_y_L_1_F + k.th_y_L_2_F) / 2.;
   	
 	k.th_y_R_1_F = + h.R_1_F.y / env.L_y_R_1_F;
   	k.th_y_R_2_F = + h.R_2_F.y / env.L_y_R_2_F;
-  	k.th_y_R = (k.th_y_R_1_F + k.th_y_R_2_F) / 2.;
+  	//k.th_y_R = (k.th_y_R_1_F + k.th_y_R_2_F) / 2.;
 	
-	double D_y_L = - env.v_y_L_1_F * env.L_y_L_2_F + env.v_y_L_2_F * env.L_y_L_1_F;
-	//k.th_y_L = (env.v_y_L_1_F * h.L_2_F.y - env.v_y_L_2_F * h.L_1_F.y) / D_y_L;
+	const double D_y_L = - env.v_y_L_1_F * env.L_y_L_2_F + env.v_y_L_2_F * env.L_y_L_1_F;
+	k.th_y_L = (env.v_y_L_1_F * h.L_2_F.y - env.v_y_L_2_F * h.L_1_F.y) / D_y_L;
 	k.vtx_y_L = (- h.L_1_F.y * env.L_y_L_2_F + h.L_2_F.y * env.L_y_L_1_F) / D_y_L;
 
-	double D_y_R = + env.v_y_R_1_F * env.L_y_R_2_F - env.v_y_R_2_F * env.L_y_R_1_F;
-	//k.th_y_R = (env.v_y_R_1_F * h.R_2_F.y - env.v_y_R_2_F * h.R_1_F.y) / D_y_R;
+	const double D_y_R = + env.v_y_R_1_F * env.L_y_R_2_F - env.v_y_R_2_F * env.L_y_R_1_F;
+	k.th_y_R = (env.v_y_R_1_F * h.R_2_F.y - env.v_y_R_2_F * h.R_1_F.y) / D_y_R;
 	k.vtx_y_R = (+ h.R_1_F.y * env.L_y_R_2_F - h.R_2_F.y * env.L_y_R_1_F) / D_y_R;
 
 	// ----- double-arm kinematics reconstruction -----
@@ -449,26 +444,19 @@ unsigned int NonCollidingBunch(unsigned int /*run*/, unsigned /*bunch*/)
 
 //----------------------------------------------------------------------------------------------------
 
-bool IsZeroBias(unsigned int trigger, unsigned int /*run*/, unsigned int /*event*/)
-{
-	return ((trigger & 512) != 0);
-}
-
-//----------------------------------------------------------------------------------------------------
-
 HitData ProtonTransport(const Kinematics & k, const Environment & env)
 {
 	HitData h;
 
 	h.L_2_F.x = -env.L_x_L_2_F*k.th_x_L + env.v_x_L_2_F*k.vtx_x;
 	h.L_2_F.y = -env.L_y_L_2_F*k.th_y_L + env.v_y_L_2_F*k.vtx_y;
-                                                               
+
 	h.L_1_F.x = -env.L_x_L_1_F*k.th_x_L + env.v_x_L_1_F*k.vtx_x;
 	h.L_1_F.y = -env.L_y_L_1_F*k.th_y_L + env.v_y_L_1_F*k.vtx_y;
-	                                                           
+
 	h.R_1_F.x = +env.L_x_R_1_F*k.th_x_R + env.v_x_R_1_F*k.vtx_x;
 	h.R_1_F.y = +env.L_y_R_1_F*k.th_y_R + env.v_y_R_1_F*k.vtx_y;
-	                                                           
+
 	h.R_2_F.x = +env.L_x_R_2_F*k.th_x_R + env.v_x_R_2_F*k.vtx_x;
 	h.R_2_F.y = +env.L_y_R_2_F*k.th_y_R + env.v_y_R_2_F*k.vtx_y;
 
