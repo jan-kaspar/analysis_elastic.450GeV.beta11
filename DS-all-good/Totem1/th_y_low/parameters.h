e95c3cda011b92103cec44dc87b99665f308208a
Jan Kaspar <jan.kaspar@cern.ch>
2020-09-21 18:42:09 +0200

    Added DS-all-good.

---
+++ DS-all-good/Totem1/th_y_low/parameters.h
@@ -0,0 +1,16 @@
+#define USE_INIT_ADDITIONAL
+
+#include "../../parameters.h"
+
+void Init_additional()
+{
+	if (diagonal == d45b_56t)
+	{
+		anal.fc_G = FiducialCut({{-400E-6, 160E-6}, {+200E-6, 160E-6}, {+540E-6, 220E-6}, {+500E-6, 300E-6}, {-500E-6, 300E-6}, {-540E-6, 220E-6}});
+	}
+
+	if (diagonal == d45t_56b)
+	{
+		anal.fc_G = FiducialCut({{-200E-6, 180E-6}, {+400E-6, 180E-6}, {+520E-6, 220E-6}, {+480E-6, 300E-6}, {-480E-6, 300E-6}, {-520E-6, 220E-6}});
+	}
+}
