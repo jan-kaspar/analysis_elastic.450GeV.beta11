dfab6bcfded526375aaea0afbed3cde84bd085d1
Jan Kaspar <jan.kaspar@gmail.com>
2020-09-21 18:57:21 +0200

    Switch to input version4.

--- DS-fill7301/Totem1/input_files.h
+++ DS-fill7301/Totem1/input_files.h
@@ -8,9 +8,9 @@ std::vector<std::string> input_files;
 void InitInputFiles()
 {
 	input_files = {
-		"root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version3/run_324575.0_re_reco_Totem1.root",
-		"root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version3/run_324575.1_re_reco_Totem1.root",
-		"root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version3/run_324576.0_re_reco_Totem1.root",
-		"root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version3/run_324576.1_re_reco_Totem1.root"
+		"root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version4/run_324575.0_re_reco_Totem1.root",
+		"root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version4/run_324575.1_re_reco_Totem1.root",
+		"root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version4/run_324576.0_re_reco_Totem1.root",
+		"root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version4/run_324576.1_re_reco_Totem1.root"
 	};
 }
