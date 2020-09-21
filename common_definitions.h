8dc8dba75cd7b4af3330c7eab05281a6c917fc57
Jan Kaspar <jan.kaspar@cern.ch>
2020-09-21 18:53:39 +0200

    Symmetrised alignment application.

--- common_definitions.h
+++ common_definitions.h
@@ -287,11 +287,17 @@ struct HitData
 	{
 		HitData r;
 
-		r.L_2_F.x = L_2_F.x - al.a_L_2_F * L_2_F.y - al.b_L_2_F; r.L_2_F.y = L_2_F.y - al.c_L_2_F;
-		r.L_1_F.x = L_1_F.x - al.a_L_1_F * L_1_F.y - al.b_L_1_F; r.L_1_F.y = L_1_F.y - al.c_L_1_F;
+		r.L_2_F.x = L_2_F.x - al.a_L_2_F * L_2_F.y - al.b_L_2_F;
+		r.L_2_F.y = L_2_F.y + al.a_L_2_F * L_2_F.x - al.c_L_2_F;
 
-		r.R_1_F.x = R_1_F.x - al.a_R_1_F * R_1_F.y - al.b_R_1_F; r.R_1_F.y = R_1_F.y - al.c_R_1_F;
-		r.R_2_F.x = R_2_F.x - al.a_R_2_F * R_2_F.y - al.b_R_2_F; r.R_2_F.y = R_2_F.y - al.c_R_2_F;
+		r.L_1_F.x = L_1_F.x - al.a_L_1_F * L_1_F.y - al.b_L_1_F;
+		r.L_1_F.y = L_1_F.y + al.a_L_1_F * L_1_F.x - al.c_L_1_F;
+
+		r.R_1_F.x = R_1_F.x - al.a_R_1_F * R_1_F.y - al.b_R_1_F;
+		r.R_1_F.y = R_1_F.y + al.a_R_1_F * R_1_F.x - al.c_R_1_F;
+
+		r.R_2_F.x = R_2_F.x - al.a_R_2_F * R_2_F.y - al.b_R_2_F;
+		r.R_2_F.y = R_2_F.y + al.a_R_2_F * R_2_F.x - al.c_R_2_F;
 
 		return r;
 	}
