596f67716dd48edef892bc6fac3daabf51ab6cd6
Jan Kaspar <jan.kaspar@cern.ch>
2021-02-12 15:02:41 +0100

    switch to input ntuples version 5

--- data/fill7302/Totem1/input_files.py
+++ data/fill7302/Totem1/input_files.py
@@ -1,18 +1,18 @@
 import FWCore.ParameterSet.Config as cms
 
 input_files = cms.vstring(
-  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version4/run_324578.0_re_reco_Totem1.root",
-  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version4/run_324578.1_re_reco_Totem1.root",
-  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version4/run_324578.2_re_reco_Totem1.root",
-  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version4/run_324578.3_re_reco_Totem1.root",
-  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version4/run_324578.4_re_reco_Totem1.root",
-  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version4/run_324578.5_re_reco_Totem1.root",
-  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version4/run_324578.6_re_reco_Totem1.root",
-  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version4/run_324578.7_re_reco_Totem1.root",
-  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version4/run_324579.0_re_reco_Totem1.root",
-  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version4/run_324579.1_re_reco_Totem1.root",
-  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version4/run_324579.2_re_reco_Totem1.root",
-  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version4/run_324580.0_re_reco_Totem1.root",
-  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version4/run_324580.1_re_reco_Totem1.root",
-  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version4/run_324581.0_re_reco_Totem1.root"
+  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version5/run_324578.0_re_reco_Totem1.root",
+  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version5/run_324578.1_re_reco_Totem1.root",
+  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version5/run_324578.2_re_reco_Totem1.root",
+  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version5/run_324578.3_re_reco_Totem1.root",
+  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version5/run_324578.4_re_reco_Totem1.root",
+  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version5/run_324578.5_re_reco_Totem1.root",
+  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version5/run_324578.6_re_reco_Totem1.root",
+  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version5/run_324578.7_re_reco_Totem1.root",
+  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version5/run_324579.0_re_reco_Totem1.root",
+  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version5/run_324579.1_re_reco_Totem1.root",
+  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version5/run_324579.2_re_reco_Totem1.root",
+  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version5/run_324580.0_re_reco_Totem1.root",
+  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version5/run_324580.1_re_reco_Totem1.root",
+  "root://eostotem.cern.ch///eos/totem/data/offline/2018/450GeV/beta11/Totem1/version5/run_324581.0_re_reco_Totem1.root"
 )
