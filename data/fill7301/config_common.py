93eb1ace7c8fc61ac45354cedef8418ca4dcf598
Jan Kaspar <jan.kaspar@cern.ch>
2021-02-24 12:43:23 +0100

    adjust alignment and cuts

--- data/fill7301/config_common.py
+++ data/fill7301/config_common.py
@@ -55,8 +55,8 @@ for c in [cfg_45b_56b, cfg_45t_56b]:
       data = cms.VPSet(
         cms.PSet(unit=cms.string("L_2_F"), a = cms.double(0E-3), b = cms.double(+100E-3 - 32E-3), c = cms.double(+250E-3 - 0.8 * 0.465)),
         cms.PSet(unit=cms.string("L_1_F"), a = cms.double(0E-3), b = cms.double(+450E-3 + 32E-3), c = cms.double( -50E-3 - 0.8 * 0.422)),
-        cms.PSet(unit=cms.string("R_1_F"), a = cms.double(0E-3), b = cms.double(+150E-3 - 10E-3), c = cms.double(-300E-3 - 1.1 * 0.438)),
-        cms.PSet(unit=cms.string("R_2_F"), a = cms.double(0E-3), b = cms.double(+850E-3 + 10E-3), c = cms.double(-200E-3 - 1.1 * 0.399))
+        cms.PSet(unit=cms.string("R_1_F"), a = cms.double(0E-3), b = cms.double(+150E-3 - 10E-3), c = cms.double(-300E-3 + 0.8 * 0.399)),
+        cms.PSet(unit=cms.string("R_2_F"), a = cms.double(0E-3), b = cms.double(+850E-3 + 10E-3), c = cms.double(-200E-3 + 0.8 * 0.438))
       )
     )
   )
@@ -64,11 +64,11 @@ for c in [cfg_45b_56b, cfg_45t_56b]:
   c.anal.cut1_c = -2E-6
   c.anal.cut2_c = +3E-6
 
-  c.anal.cut5_c = -0.04
-  c.anal.cut6_c = +0.03
+  c.anal.cut5_c = -0.00
+  c.anal.cut6_c = +0.00
 
   c.anal.cut7_c = -0.01
-  c.anal.cut8_c = +0.22
+  c.anal.cut8_c = +0.00
 
   c.anal.cut9_c = +0.02
   c.anal.cut10_c = +0.00
--- data/fill7302/config_common.py
+++ data/fill7302/config_common.py
@@ -55,8 +55,8 @@ for c in [cfg_45b_56b, cfg_45t_56b]:
       data = cms.VPSet(
         cms.PSet(unit=cms.string("L_2_F"), a = cms.double(0E-3), b = cms.double(+300E-3 - 20E-3), c = cms.double(+250E-3 - 0.2 * 0.465)),
         cms.PSet(unit=cms.string("L_1_F"), a = cms.double(0E-3), b = cms.double(+700E-3 + 20E-3), c = cms.double( -50E-3 - 0.2 * 0.422)),
-        cms.PSet(unit=cms.string("R_1_F"), a = cms.double(0E-3), b = cms.double(-200E-3 + 20E-3), c = cms.double(-300E-3 - 0.4 * 0.438)),
-        cms.PSet(unit=cms.string("R_2_F"), a = cms.double(0E-3), b = cms.double(+600E-3 - 20E-3), c = cms.double(-200E-3 - 0.4 * 0.399))
+        cms.PSet(unit=cms.string("R_1_F"), a = cms.double(0E-3), b = cms.double(-200E-3 + 20E-3), c = cms.double(-300E-3 + 0.3 * 0.399)),
+        cms.PSet(unit=cms.string("R_2_F"), a = cms.double(0E-3), b = cms.double(+600E-3 - 20E-3), c = cms.double(-200E-3 + 0.3 * 0.438))
       )
     )
   )
@@ -64,11 +64,11 @@ for c in [cfg_45b_56b, cfg_45t_56b]:
   c.anal.cut1_c = +0E-6
   c.anal.cut2_c = +3E-6
 
-  c.anal.cut5_c = -0.02
-  c.anal.cut6_c = +0.009
+  c.anal.cut5_c = -0.01
+  c.anal.cut6_c = +0.00
 
   c.anal.cut7_c = -0.01
-  c.anal.cut8_c = +0.09
+  c.anal.cut8_c = +0.02
 
   c.anal.cut9_c = +0.02
   c.anal.cut10_c = +0.02
