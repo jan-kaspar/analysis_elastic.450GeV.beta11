9594cd148a8cac6cbda596bbab06a4c280023e8d
Jan Kaspar <jan.kaspar@cern.ch>
2021-03-03 15:46:14 +0100

    retune R-L and N-F alignment, cuts

--- data/fill7301/config_common.py
+++ data/fill7301/config_common.py
@@ -33,25 +33,25 @@ for c in [cfg_45t_56t, cfg_45b_56t]:
   c.anal.alignment_sources = cms.VPSet(
     cms.PSet(
       data = cms.VPSet(
-        cms.PSet(unit=cms.string("L_2_F"), a = cms.double(a_L_2_F + 0E-3), b = cms.double(b_L_2_F - 0E-3), c = cms.double(c_L_2_F + 0E-3)),
-        cms.PSet(unit=cms.string("L_1_F"), a = cms.double(a_L_1_F + 0E-3), b = cms.double(b_L_1_F + 0E-3), c = cms.double(c_L_1_F + 0E-3)),
-        cms.PSet(unit=cms.string("R_1_F"), a = cms.double(a_R_1_F + 0E-3), b = cms.double(b_R_1_F - 0E-3), c = cms.double(c_R_1_F + 0E-3)),
-        cms.PSet(unit=cms.string("R_2_F"), a = cms.double(a_R_2_F + 0E-3), b = cms.double(b_R_2_F + 0E-3), c = cms.double(c_R_2_F + 0E-3))
+        cms.PSet(unit=cms.string("L_2_F"), a = cms.double(a_L_2_F + 0E-3), b = cms.double(b_L_2_F - 28E-3), c = cms.double(c_L_2_F + 5E-3)),
+        cms.PSet(unit=cms.string("L_1_F"), a = cms.double(a_L_1_F + 0E-3), b = cms.double(b_L_1_F + 28E-3), c = cms.double(c_L_1_F - 5E-3)),
+        cms.PSet(unit=cms.string("R_1_F"), a = cms.double(a_R_1_F + 0E-3), b = cms.double(b_R_1_F - 12E-3), c = cms.double(c_R_1_F + 30E-3)),
+        cms.PSet(unit=cms.string("R_2_F"), a = cms.double(a_R_2_F + 0E-3), b = cms.double(b_R_2_F + 12E-3), c = cms.double(c_R_2_F - 30E-3))
       )
     )
   )
 
-  c.anal.cut1_c = +17.1E-6
-  c.anal.cut2_c = +23.0E-6
+  c.anal.cut1_c = +2.9E-6
+  c.anal.cut2_c = +0.6E-6
 
-  c.anal.cut5_c = +0.045
-  c.anal.cut6_c = +0.00
+  c.anal.cut5_c = -0.015
+  c.anal.cut6_c = +0.01
 
-  c.anal.cut7_c = +0.075
-  c.anal.cut8_c = -0.13
+  c.anal.cut7_c = +0.00
+  c.anal.cut8_c = +0.06
 
-  c.anal.cut9_c = -0.03
-  c.anal.cut10_c = +0.07
+  c.anal.cut9_c = -0.00
+  c.anal.cut10_c = -0.005
 
   c.anal.L_int = 1
 
@@ -61,24 +61,24 @@ for c in [cfg_45b_56b, cfg_45t_56b]:
   c.anal.alignment_sources = cms.VPSet(
     cms.PSet(
       data = cms.VPSet(
-        cms.PSet(unit=cms.string("L_2_F"), a = cms.double(a_L_2_F + 0E-3), b = cms.double(b_L_2_F - 0E-3), c = cms.double(c_L_2_F - 0. * 0.465)),
-        cms.PSet(unit=cms.string("L_1_F"), a = cms.double(a_L_1_F + 0E-3), b = cms.double(b_L_1_F + 0E-3), c = cms.double(c_L_1_F - 0. * 0.422)),
-        cms.PSet(unit=cms.string("R_1_F"), a = cms.double(a_R_1_F + 0E-3), b = cms.double(b_R_1_F - 0E-3), c = cms.double(c_R_1_F + 0. * 0.399)),
-        cms.PSet(unit=cms.string("R_2_F"), a = cms.double(a_R_2_F + 0E-3), b = cms.double(b_R_2_F + 0E-3), c = cms.double(c_R_2_F + 0. * 0.438))
+        cms.PSet(unit=cms.string("L_2_F"), a = cms.double(a_L_2_F + 0E-3), b = cms.double(b_L_2_F - 28E-3), c = cms.double(c_L_2_F - 26E-3)),
+        cms.PSet(unit=cms.string("L_1_F"), a = cms.double(a_L_1_F + 0E-3), b = cms.double(b_L_1_F + 28E-3), c = cms.double(c_L_1_F + 26E-3)),
+        cms.PSet(unit=cms.string("R_1_F"), a = cms.double(a_R_1_F + 0E-3), b = cms.double(b_R_1_F - 12E-3), c = cms.double(c_R_1_F - 4E-3)),
+        cms.PSet(unit=cms.string("R_2_F"), a = cms.double(a_R_2_F + 0E-3), b = cms.double(b_R_2_F + 12E-3), c = cms.double(c_R_2_F + 4E-3))
       )
     )
   )
 
-  c.anal.cut1_c = +11.0E-6
-  c.anal.cut2_c = +20.1E-6
+  c.anal.cut1_c = -3.6E-6
+  c.anal.cut2_c = -0.7E-6
 
-  c.anal.cut5_c = +0.01
-  c.anal.cut6_c = +0.03
+  c.anal.cut5_c = +0.02
+  c.anal.cut6_c = -0.02
 
-  c.anal.cut7_c = +0.08
-  c.anal.cut8_c = +0.15
+  c.anal.cut7_c = +0.00
+  c.anal.cut8_c = -0.04
 
-  c.anal.cut9_c = -0.02
-  c.anal.cut10_c = +0.085
+  c.anal.cut9_c = 0.01
+  c.anal.cut10_c = +0.015
 
   c.anal.L_int = 1
--- data/fill7302/config_common.py
+++ data/fill7302/config_common.py
@@ -33,25 +33,25 @@ for c in [cfg_45t_56t, cfg_45b_56t]:
   c.anal.alignment_sources = cms.VPSet(
     cms.PSet(
       data = cms.VPSet(
-        cms.PSet(unit=cms.string("L_2_F"), a = cms.double(a_L_2_F + 0E-3), b = cms.double(b_L_2_F - 0E-3), c = cms.double(c_L_2_F + 0E-3)),
-        cms.PSet(unit=cms.string("L_1_F"), a = cms.double(a_L_1_F + 0E-3), b = cms.double(b_L_1_F + 0E-3), c = cms.double(c_L_1_F + 0E-3)),
-        cms.PSet(unit=cms.string("R_1_F"), a = cms.double(a_R_1_F + 0E-3), b = cms.double(b_R_1_F - 0E-3), c = cms.double(c_R_1_F + 0E-3)),
-        cms.PSet(unit=cms.string("R_2_F"), a = cms.double(a_R_2_F + 0E-3), b = cms.double(b_R_2_F + 0E-3), c = cms.double(c_R_2_F + 0E-3))
+        cms.PSet(unit=cms.string("L_2_F"), a = cms.double(a_L_2_F + 0E-3), b = cms.double(b_L_2_F - 20E-3), c = cms.double(c_L_2_F + 5E-3)),
+        cms.PSet(unit=cms.string("L_1_F"), a = cms.double(a_L_1_F + 0E-3), b = cms.double(b_L_1_F + 20E-3), c = cms.double(c_L_1_F - 5E-3)),
+        cms.PSet(unit=cms.string("R_1_F"), a = cms.double(a_R_1_F + 0E-3), b = cms.double(b_R_1_F +  3E-3), c = cms.double(c_R_1_F + 35E-3)),
+        cms.PSet(unit=cms.string("R_2_F"), a = cms.double(a_R_2_F + 0E-3), b = cms.double(b_R_2_F -  3E-3), c = cms.double(c_R_2_F - 35E-3))
       )
     )
   )
 
-  c.anal.cut1_c = +21.3E-6
-  c.anal.cut2_c = +26.2E-6
+  c.anal.cut1_c = +0.4E-6
+  c.anal.cut2_c = -0.6E-6
 
-  c.anal.cut5_c = +0.07
-  c.anal.cut6_c = -0.009
+  c.anal.cut5_c = -0.003
+  c.anal.cut6_c = -0.003
 
-  c.anal.cut7_c = +0.04
-  c.anal.cut8_c = -0.23
+  c.anal.cut7_c = +0.005
+  c.anal.cut8_c = -0.008
 
-  c.anal.cut9_c = +0.005
-  c.anal.cut10_c = +0.05
+  c.anal.cut9_c = -0.003
+  c.anal.cut10_c = +0.00
 
   c.anal.L_int = 1
 
@@ -61,24 +61,24 @@ for c in [cfg_45b_56b, cfg_45t_56b]:
   c.anal.alignment_sources = cms.VPSet(
     cms.PSet(
       data = cms.VPSet(
-        cms.PSet(unit=cms.string("L_2_F"), a = cms.double(a_L_2_F + 0E-3), b = cms.double(b_L_2_F - 0E-3), c = cms.double(c_L_2_F - 0. * 0.465)),
-        cms.PSet(unit=cms.string("L_1_F"), a = cms.double(a_L_1_F + 0E-3), b = cms.double(b_L_1_F + 0E-3), c = cms.double(c_L_1_F - 0. * 0.422)),
-        cms.PSet(unit=cms.string("R_1_F"), a = cms.double(a_R_1_F + 0E-3), b = cms.double(b_R_1_F - 0E-3), c = cms.double(c_R_1_F + 0. * 0.399)),
-        cms.PSet(unit=cms.string("R_2_F"), a = cms.double(a_R_2_F + 0E-3), b = cms.double(b_R_2_F + 0E-3), c = cms.double(c_R_2_F + 0. * 0.438))
+        cms.PSet(unit=cms.string("L_2_F"), a = cms.double(a_L_2_F + 0E-3), b = cms.double(b_L_2_F - 23E-3), c = cms.double(c_L_2_F - 15E-3)),
+        cms.PSet(unit=cms.string("L_1_F"), a = cms.double(a_L_1_F + 0E-3), b = cms.double(b_L_1_F + 23E-3), c = cms.double(c_L_1_F + 15E-3)),
+        cms.PSet(unit=cms.string("R_1_F"), a = cms.double(a_R_1_F + 0E-3), b = cms.double(b_R_1_F -  1E-3), c = cms.double(c_R_1_F +  8E-3)),
+        cms.PSet(unit=cms.string("R_2_F"), a = cms.double(a_R_2_F + 0E-3), b = cms.double(b_R_2_F +  1E-3), c = cms.double(c_R_2_F -  8E-3))
       )
     )
   )
 
-  c.anal.cut1_c = +16.7E-6
-  c.anal.cut2_c = +20.0E-6
+  c.anal.cut1_c = -3.3E-6
+  c.anal.cut2_c = -1.3E-6
 
-  c.anal.cut5_c = +0.03
-  c.anal.cut6_c = +0.006
+  c.anal.cut5_c = +0.010
+  c.anal.cut6_c = -0.020
 
-  c.anal.cut7_c = +0.05
-  c.anal.cut8_c = +0.03
+  c.anal.cut7_c = +0.004
+  c.anal.cut8_c = -0.020
 
   c.anal.cut9_c = +0.005
-  c.anal.cut10_c = +0.07
+  c.anal.cut10_c = +0.017
 
   c.anal.L_int = 1
