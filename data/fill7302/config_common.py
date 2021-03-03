7e2b3dc1d28eb2f0fb75cadb210ec3d75aba5b9b
Jan Kaspar <jan.kaspar@cern.ch>
2021-03-03 07:09:32 +0100

    use 4 alignment periods

--- data/fill7302/config_common.py
+++ data/fill7302/config_common.py
@@ -17,7 +17,7 @@ for c in all_cfg:
   c.timber_dir = "2018_10_14_fill7302"
 
   c.anal.alignment_t0 = timestamp_min
-  c.anal.alignment_ts = (timestamp_max - timestamp_min) / 2.
+  c.anal.alignment_ts = (timestamp_max - timestamp_min) / 4.
 
 #--------------------
 
